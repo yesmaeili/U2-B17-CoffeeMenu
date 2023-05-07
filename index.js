8ee6b8eec15cc5076e72f55393d0311b7d10f33f
Yasaman Esmaeili <yasamanesmaeili@Yasamans-MacBook-Air.local>
2023-05-07 14:30:18 -0400

    feat: all commants are updated.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Connect two files.
 const coffeeMenu = [
     {
         name: "cappuccino",
@@ -45,5 +46,29 @@ const coffeeMenu = [
         seasonal: true,
     }
 ];
+
+//Return an array of all the drinks on the menu.
+const drinks = coffeeMenu.map(drink => drink.name);
+console.log(drinks);
+
+//Return an array of drinks that cost 5 and under.
+const cheapDrinks = coffeeMenu.filter(drink => drink.price <= 5);
+console.log(cheapDrinks);
+
+//Return an array of drinks that are priced at an even number.
+const evenPricedDrinks = coffeeMenu.filter(drink => drink.price % 2 === 0);
+console.log(evenPricedDrinks);
+
+//Return the total if you were to order one of every drink.
+const total = coffeeMenu.reduce((acc, drink) => acc + Number(drink.price), 0);
+console.log(total);
+
+//Return an array with all the drinks that are seasonal.
+const seasonalDrinks = coffeeMenu.filter(drink => drink.seasonal === true);
+console.log(seasonalDrinks);
+
+//Return all the seasonal drinks with the words "with imported beans" after the item name.
+const seasonalImportedDrinks = seasonalDrinks.map(drink => `${drink.name} with imported beans`);
+console.log(seasonalImportedDrinks);
 
 module.exports = coffeeMenu;
